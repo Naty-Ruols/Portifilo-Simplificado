5e29e6ee1d2d2da502c53464852a9e67546efd7d
Natalia Cerqueira <153464979+Naty-Ruols@users.noreply.github.com>
2024-04-20 21:50:07 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,13 @@
+const botaoMostrarProjetos = document.querySelector('.btn-mostrar-projetos');
+const projetosInativos = document.querySelectorAll('.projeto:not(.ativo)')
+
+botaoMostrarProjetos.addEventListener('click', () => {
+    newFunction();
+    botaoMostrarProjetos.classList.add("remover")
+})
+
+function newFunction() {
+    projetosInativos.forEach(projetoInativo => {
+        projetoInativo.classList.add('ativo');
+    });
+}
